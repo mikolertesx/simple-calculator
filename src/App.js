6ae13a2ba611b6aab4ecac6a62a636529386b810
Miguel Angel <mgangsal@outlook.com>
2020-09-25 12:53:14 -0500

    Programmed basic functions.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
-import './App.css';
+import "./App.css";
 
 // Components.
 import Display from "./components/display";
 import NumericButton from "./components/numericButton";
-import { appLayout } from "./layout";
+import OperationButton from "./components/operationButton";
+
+import operators from "./utils/operators";
+import { appLayout } from "./utils/layout";
 
 function App() {
+  const [display, setDisplay] = useState(null);
+  const [currentValue, setCurrentValue] = useState(null);
+
+  const operationButtonHandler = (operator) => {
+    if (operators[operator]) {
+      const operation = operators[operator];
+      // Do the math.
+      if (currentValue === null) {
+        setCurrentValue(+display);
+        setDisplay(null);
+      } else {
+        setCurrentValue((prevValue) => {
+          setDisplay(null);
+          return operation(prevValue, +display);
+        });
+      }
+    } else {
+      // Do the procedure.
+    }
+  };
+
+  const numericButtonHandler = (number) => {
+    setDisplay((prevDisplay) =>
+      prevDisplay === null ? number.toString() : prevDisplay + number.toString()
+    );
+  };
+
   return (
     <div className="App">
-      <Display/>
+      <Display text={display || currentValue} />
       <div className="calc-buttons">
-        {appLayout.map((value) => (
-          <NumericButton key={`btn-${value}`} text={value} />
-        ))}
+        {appLayout.map((value) => {
+          return isNaN(+value) ? (
+            <OperationButton
+              onClick={() => operationButtonHandler(value)}
+              key={`btn-${value}`}
+              text={value}
+            />
+          ) : (
+            <NumericButton
+              onClick={() => numericButtonHandler(value)}
+              key={`btn-${value}`}
+              text={value}
+            />
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/utils/operators.js
@@ -0,0 +1,9 @@
+const operations = {
+  '+': (num1, num2) => num1 + num2,
+  '-': (num1, num2) => num1 - num2,
+  '/': (num1, num2) => num1 / num2,
+  'X': (num1, num2) => num1 * num2,
+  '%': (num1, num2) => num1 % num2,
+}
+
+export default operations;
