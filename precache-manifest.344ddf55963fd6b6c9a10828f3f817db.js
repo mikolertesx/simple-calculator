9ef486d65c9259f808e5f06084c9c94c19db369d
Miguel Angel <mgangsal@outlook.com>
2020-09-25 13:45:17 -0500

    Updates

---
+++ precache-manifest.344ddf55963fd6b6c9a10828f3f817db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5764cc9a7d817abfefc9857a104944a2",
+    "url": "/mikolertesx/simple-calculator/index.html"
+  },
+  {
+    "revision": "cc72643c92d6d27713c3",
+    "url": "/mikolertesx/simple-calculator/static/css/main.6f3310bc.chunk.css"
+  },
+  {
+    "revision": "ecada51e1a3334c42b99",
+    "url": "/mikolertesx/simple-calculator/static/js/2.e6f076d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mikolertesx/simple-calculator/static/js/2.e6f076d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc72643c92d6d27713c3",
+    "url": "/mikolertesx/simple-calculator/static/js/main.8065c8dd.chunk.js"
+  },
+  {
+    "revision": "9049b2b0df27b9868131",
+    "url": "/mikolertesx/simple-calculator/static/js/runtime-main.bd1318de.js"
+  }
+]);
