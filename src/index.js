3cd1421a1ce9091bc9a8a5b09a318034449f0506
Miguel Angel <mgangsal@outlook.com>
2020-09-25 13:38:30 -0500

    Added some basic styling.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <p className="warning-resolution">Resolution is too low to show a calculator.</p>
   </React.StrictMode>,
   document.getElementById('root')
 );
